fff0dfc9c40fc7b02aef4f0f00360c98b3d36a2d
AtharvaKeny <atharvakeny0914@gmail.com>
2023-12-18 12:18:05 +0530

    Final

---
+++ BookWorm-C#/Controllers/RoyaltyCalculationsController.cs
@@ -0,0 +1,64 @@
+﻿using BookWorm_C_.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookWorm_C_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoyaltyCalculationsController : ControllerBase
+    {
+        private readonly IRoyaltyCalculationRepository _repository;
+
+        public RoyaltyCalculationsController(IRoyaltyCalculationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoyaltyCalculation>>> GetRoyaltyCalculations()
+        {
+            var royaltyCalculations = await _repository.GetAllAsync();
+            return Ok(royaltyCalculations);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RoyaltyCalculation>> GetRoyaltyCalculation(long id)
+        {
+            var royaltyCalculation = await _repository.GetByIdAsync(id);
+            if (royaltyCalculation == null)
+                return NotFound();
+
+            return Ok(royaltyCalculation);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RoyaltyCalculation>> PostRoyaltyCalculation(RoyaltyCalculation royaltyCalculation)
+        {
+            var createdRoyaltyCalculation = await _repository.CreateAsync(royaltyCalculation);
+            return CreatedAtAction(nameof(GetRoyaltyCalculation), new { id = createdRoyaltyCalculation.RoyaltyCalculationId }, createdRoyaltyCalculation);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRoyaltyCalculation(long id, RoyaltyCalculation royaltyCalculation)
+        {
+            var updatedRoyaltyCalculation = await _repository.UpdateAsync(id, royaltyCalculation);
+            if (updatedRoyaltyCalculation == null)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRoyaltyCalculation(long id)
+        {
+            var result = await _repository.DeleteAsync(id);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
